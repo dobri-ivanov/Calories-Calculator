4e8ff42337418ebf2957f3bd63c3be2a0d76d989
dobri-ivanov <dobri0316@abv.bg>
2022-10-10 15:48:32 +0300

    Redisigned

--- Result.Designer.cs
+++ Result.Designer.cs
@@ -51,7 +51,6 @@
             // 
             // buttonClose
             // 
-            this.buttonClose.BackColor = System.Drawing.SystemColors.ControlDark;
             this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonClose.ForeColor = System.Drawing.Color.Black;
             this.buttonClose.Location = new System.Drawing.Point(315, 323);
